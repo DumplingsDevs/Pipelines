ace902f84b8bf147a6a3bcad307ce09b26afd26c
Karasini <Karasini@users.noreply.github.com>
2023-06-08 15:51:34 +0200

    Refactor HandleExecutedMethod, Validators in progress

--- src/Pipelines/DispatcherInterceptor.cs
+++ src/Pipelines/DispatcherInterceptor.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Castle.DynamicProxy;
 
 namespace Pipelines;
@@ -31,34 +32,65 @@ public class DispatcherInterceptor : IInterceptor
 
     private object? HandleExecutedMethod(object[] args)
     {
-        // for now get first - but we should filter args and look for object that implements input interface - or just try to match the best method
-        var input = args.FirstOrDefault();
-        var inputType = input.GetType();
-        var resultType = GetResultType(inputType);
+        ValidateArgs(args);
+        
+        var inputType = args.First().GetType();
+        
+        var handler = GetHandlerService(inputType);
 
-        Type handlerTypeWithInput;
-        if (resultType is null)
-        {
-            handlerTypeWithInput = _handlerType.MakeGenericType(inputType);
-        }
-        else
-        {
-            var inputAndResultTypes = new[] { inputType, resultType };
-            handlerTypeWithInput = _handlerType.MakeGenericType(inputAndResultTypes);
-        }
-
-        var handler = _serviceProvider.GetService(handlerTypeWithInput);
-
-        // What if handler contains more items 
-        var method = handler.GetType().GetMethods()
-            .FirstOrDefault(x => x.GetParameters().Any(y => y.ParameterType == inputType));
+        var method = GetHandlerMethod(handler, inputType);
 
         return method.Invoke(handler, args);
+    }
 
+    private static MethodInfo GetHandlerMethod(object handler, Type inputType)
+    {
+        var methods = handler.GetType()
+            .GetMethods()
+            .Where(x => x.GetParameters()
+                .Any(y => y.ParameterType == inputType))
+            .ToList();
 
-        // find handler
-        // trigger method
-        //return result
+        switch (methods.Count)
+        {
+            case 0:
+                throw new Exception($"(TODO) Cannot find method in {handler.GetType().Name} which accepts input of type {inputType.Name}");
+            case > 1:
+                throw new Exception($"(TODO) handler should implement only one method with input of type {inputType}");
+        }
+
+        var method = methods.First();
+
+        // think how to validate method because basically we have two types of method:
+        // - only with input
+        // - with input and CancellationToken
+
+        return method;
+    }
+
+    private object GetHandlerService(Type inputType)
+    {
+        var resultType = GetResultType(inputType);
+
+        var handlerTypeWithInput = resultType is null
+            ? _handlerType.MakeGenericType(inputType)
+            : _handlerType.MakeGenericType(inputType, resultType);
+
+        var handler =  _serviceProvider.GetService(handlerTypeWithInput);
+        if (handler is null)
+        {
+            throw new Exception("(TODO) Handler is not registered");
+        }
+
+        return handler;
+    }
+
+    private void ValidateArgs(object[] args)
+    {
+        // Exception when args == 0 || args > 1
+        // var input = args.FirstOrDefault();
+        // Exception when input is null
+        // Exception when input.GetType != _inputType 
     }
 
     private static Type? GetResultType(Type queryType)
