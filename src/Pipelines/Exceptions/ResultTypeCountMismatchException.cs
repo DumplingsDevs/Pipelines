17ad24a824c31f11ec3299d836de2ada44bdd838
KmLPro <kamil.bytner@gmail.com>
2023-06-24 00:31:19 +0200

    Added new exceptions that will be used in ValidateHandlerHandleMethod, removed unused

---
+++ src/Pipelines/Exceptions/ResultTypeCountMismatchException.cs
@@ -0,0 +1,9 @@
+namespace Pipelines.Exceptions;
+
+public class ResultTypeCountMismatchException : Exception
+{
+    private const string ErrorMessageFormat = "Expected {0} result type(s), but found {1}";
+
+    public ResultTypeCountMismatchException(int expected, int found) 
+        : base(string.Format(ErrorMessageFormat, expected, found)) { }
+}
