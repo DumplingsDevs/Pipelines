0701d761703da28cac4402eacfe925fa8542644a
Karasini <Karasini@users.noreply.github.com>
2023-08-02 18:18:34 +0200

    Generate dispatcher's method based on interface

--- Pipelines.Benchmarks/Types/IRequestDispatcher.cs
+++ Pipelines.Benchmarks/Types/IRequestDispatcher.cs
@@ -8,24 +8,24 @@ public interface IRequestDispatcher
     public Task<TResult> SendAsync<TResult>(IRequest<TResult> request, CancellationToken token) where TResult : class;
 }
 
-// class Example : IRequestDispatcher
-// {
-//     private readonly IServiceProvider _serviceProvider;
-//
-//     public Example(IServiceProvider serviceProvider)
-//     {
-//         _serviceProvider = serviceProvider;
-//     }
-//
-//     public async Task<TResult> SendAsync<TResult>(IRequest<TResult> request, CancellationToken token)
-//         where TResult : class
-//
-//     {
-//         switch (request)
-//         {
-//             case ExampleRequest r: return (await _serviceProvider.GetRequiredService<IRequestHandler<ExampleRequest, ExampleCommandResult>>().HandleAsync(r, token)) as TResult;
-//         }
-//
-//         throw new Exception();
-//     }
-// }
+class Example : IRequestDispatcher
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public Example(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public async Task<TResult> SendAsync<TResult>(IRequest<TResult> request, CancellationToken token)
+        where TResult : class
+
+    {
+        switch (request)
+        {
+            case ExampleRequest r: return (await _serviceProvider.GetRequiredService<IRequestHandler<ExampleRequest, ExampleCommandResult>>().HandleAsync(r, token)) as TResult;
+        }
+
+        throw new Exception();
+    }
+}
