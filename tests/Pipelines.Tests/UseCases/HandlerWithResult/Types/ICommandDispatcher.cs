9eda089317a8e5ee4140b6a2b69d5cf6df1d947b
Karasini <Karasini@users.noreply.github.com>
2023-08-04 18:09:44 +0200

    Correct dispatcher generator to generate types with namespaces

--- tests/Pipelines.Tests/UseCases/HandlerWithResult/Types/ICommandDispatcher.cs
+++ tests/Pipelines.Tests/UseCases/HandlerWithResult/Types/ICommandDispatcher.cs
@@ -3,5 +3,5 @@ namespace Pipelines.Tests.UseCases.HandlerWithResult.Types;
 [GenerateImplementation]
 public interface ICommandDispatcher
 {
-    public Task<TResult> SendAsync<TResult>(ICommand<TResult> command, CancellationToken token);
+    public Task<TResult> SendAsync<TResult>(ICommand<TResult> command, CancellationToken token) where TResult : class;
 }
