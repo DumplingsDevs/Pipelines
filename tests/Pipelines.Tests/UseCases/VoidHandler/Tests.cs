07f02311b825426e9df0e35a6ba26a1b9faed033
KmLPro <kamil.bytner@gmail.com>
2023-08-06 22:02:18 +0200

    Added decorators for VoidHandler test Use case

--- tests/Pipelines.Tests/UseCases/VoidHandler/Tests.cs
+++ tests/Pipelines.Tests/UseCases/VoidHandler/Tests.cs
@@ -7,16 +7,27 @@ public class Tests
 {
     private readonly ICommandDispatcher _commandDispatcher;
     private readonly DependencyContainer _dependencyContainer;
+    private readonly DecoratorsState _state;
+
     public Tests()
     {
         _dependencyContainer = new DependencyContainer();
         var assembly = typeof(DependencyContainer).Assembly;
+
+        _dependencyContainer.RegisterPipeline<ICommandDispatcher>(assembly, typeof(ICommand), typeof(ICommandHandler<>),
+            builder =>
+            {
+                builder
+                    .WithOpenTypeDecorator(typeof(LoggingDecorator<>));
+            });
         
-        _dependencyContainer.RegisterPipeline<ICommandDispatcher>(assembly,typeof(ICommand), typeof(ICommandHandler<>));
+        _dependencyContainer.RegisterSingleton<DecoratorsState>();
+
         _dependencyContainer.BuildContainer();
         _commandDispatcher = _dependencyContainer.GetService<ICommandDispatcher>();
+        _state = _dependencyContainer.GetService<DecoratorsState>();
     }
-    
+
     [Test]
     public void HappyPath()
     {
@@ -24,6 +35,12 @@ public class Tests
         var request = new ExampleCommand("My test request");
 
         //Act & Assert
-        Assert.DoesNotThrow(()=> _commandDispatcher.SendAsync(request, new CancellationToken()));
+        Assert.DoesNotThrow(() => _commandDispatcher.SendAsync(request, new CancellationToken()));
+        
+        CollectionAssert.AreEqual(new List<string>
+        {
+            typeof(LoggingDecorator<>).Name,
+            typeof(LoggingDecorator<>).Name,
+        }, _state.Status);
     }
 }
