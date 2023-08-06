271374692280f41e7e6088a3c8720704d63c9d1a
KmLPro <kamil.bytner@gmail.com>
2023-08-06 23:27:58 +0200

    Add documentation for Build Method, Added one exception to AddDispatcher documentation

--- src/Pipelines/Builder/Interfaces/IPipelineBuildBuilder.cs
+++ src/Pipelines/Builder/Interfaces/IPipelineBuildBuilder.cs
@@ -2,5 +2,8 @@ namespace Pipelines.Builder.Interfaces;
 
 public interface IPipelineBuildBuilder
 {
+    /// <summary>
+    /// Builds the pipeline with the specified configurations. Register all types in Dependency Injection Container.
+    /// </summary>
     public void Build();
 }
