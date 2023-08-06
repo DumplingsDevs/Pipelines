b1f9d2285c364b907609cc849bc0b61ad386985f
KmLPro <kamil.bytner@gmail.com>
2023-08-06 21:59:33 +0200

    Added decorators for TaskVoidHandler

---
+++ tests/Pipelines.Tests/UseCases/TaskVoidHandler/Sample/DecoratorsState.cs
@@ -0,0 +1,6 @@
+namespace Pipelines.Tests.UseCases.TaskVoidHandler.Sample;
+
+public sealed class DecoratorsState
+{
+    public readonly List<string> Status = new();
+}
--- tests/Pipelines.Tests/UseCases/TaskVoidHandler/Tests.cs
+++ tests/Pipelines.Tests/UseCases/TaskVoidHandler/Tests.cs
@@ -7,16 +7,27 @@ public class Tests
 {
     private readonly ICommandDispatcher _commandDispatcher;
     private readonly DependencyContainer _dependencyContainer;
+    private readonly DecoratorsState _state;
+
     public Tests()
     {
         _dependencyContainer = new DependencyContainer();
         var assembly = typeof(DependencyContainer).Assembly;
-        
-        _dependencyContainer.RegisterPipeline<ICommandDispatcher>(assembly,typeof(ICommand), typeof(ICommandHandler<>));
+
+        _dependencyContainer.RegisterPipeline<ICommandDispatcher>(assembly, typeof(ICommand), typeof(ICommandHandler<>),
+            builder =>
+            {
+                builder
+                    .WithOpenTypeDecorator(typeof(LoggingDecorator<>));
+            });
+
+        _dependencyContainer.RegisterSingleton<DecoratorsState>();
+
         _dependencyContainer.BuildContainer();
         _commandDispatcher = _dependencyContainer.GetService<ICommandDispatcher>();
+        _state = _dependencyContainer.GetService<DecoratorsState>();
     }
-    
+
     [Test]
     public async Task HappyPath()
     {
@@ -27,8 +38,13 @@ public class Tests
         var result = _commandDispatcher.SendAsync(request, new CancellationToken());
         await result;
         result.Wait();
-        
+
         //Assert
         Assert.That(result.Status, Is.EqualTo(TaskStatus.RanToCompletion));
+        CollectionAssert.AreEqual(new List<string>
+        {
+            typeof(LoggingDecorator<>).Name,
+            typeof(LoggingDecorator<>).Name,
+        }, _state.Status);
     }
 }
