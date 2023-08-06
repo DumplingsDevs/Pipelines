34246a2dba74341c41d05c25226bc8585a4c7b19
KmLPro <kamil.bytner@gmail.com>
2023-08-06 23:11:10 +0200

    Add documentation for AddDispatcher

--- src/Pipelines/Builder/Interfaces/IDispatcherBuilder.cs
+++ src/Pipelines/Builder/Interfaces/IDispatcherBuilder.cs
@@ -2,6 +2,44 @@ namespace Pipelines.Builder.Interfaces;
 
 public interface IDispatcherBuilder
 {
+    /// <summary>
+    /// Adds a specific Dispatcher Interface Type to the pipeline builder. This interface will be implemented by Dispatcher (which will be provided by Pipelines library).
+    /// </summary>
+    /// <typeparam name="TDispatcher">This is the Type of the Dispatcher interface to be added to the pipeline builder. This method expects a interface Type that will be implemented by Dispatcher.</typeparam>
+    /// <returns>An IDispatcherBuilder instance that allows for further pipeline configuration.</returns>
+    /// <exception cref="Pipelines.Builder.Validators.Shared.InterfaceConstraint.Exceptions.ProvidedTypeIsNotInterfaceException"></exception>
+    /// <exception cref="Pipelines.Builder.Validators.Shared.OnlyOneHandleMethod.Exceptions.HandlerMethodNotFoundException"></exception>
+    /// <exception cref="Pipelines.Builder.Validators.Shared.OnlyOneHandleMethod.Exceptions.MultipleHandlerMethodsException"></exception>
+    /// <exception cref="Pipelines.Builder.Validators.Shared.MethodWithOneParameter.Exceptions.MethodShouldHaveAtLeastOneParameterException"></exception>
+    /// <exception cref="Pipelines.Builder.Validators.Dispatcher.InputType.Exceptions.DispatcherMethodInputTypeMismatchException"></exception>
+    /// <exception cref="Pipelines.Builder.Validators.Shared.MethodResultTypes.Exceptions.ExpectedMethodWithResultException"></exception>
+    /// <exception cref="Pipelines.Builder.Validators.Shared.MethodResultTypes.Exceptions.ExpectedVoidMethodException"></exception>
+    /// <exception cref="Pipelines.Builder.Validators.Shared.MethodResultTypes.Exceptions.ResultTypeCountMismatchException"></exception>
+    /// <exception cref="Pipelines.Builder.Validators.Shared.ShouldHaveClassConstraint.Exceptions.ReturnTypesShouldHaveClassConstraintException"></exception>
+    /// <exception cref="Pipelines.Builder.Validators.Shared.CompareTypes.Exceptions.GenericTypeCountMismatchException"></exception>
+    /// <exception cref="Pipelines.Builder.Validators.Shared.CompareTypes.Exceptions.GenericTypeMismatchException"></exception>
+    /// <exception cref="Pipelines.Builder.Validators.Shared.CompareTypes.Exceptions.IsGenericMismatchException"></exception>
+    /// <exception cref="Pipelines.Builder.Validators.Shared.CompareTypes.Exceptions.TypeMismatchException"></exception>
     public IPipelineDecoratorBuilder AddDispatcher<TDispatcher>() where TDispatcher : class;
+    
+    /// <summary>
+    /// Adds a specific Dispatcher Interface Type to the pipeline builder. This interface will be implemented by Dispatcher (which will be provided by Pipelines library).
+    /// </summary>
+    /// <typeparam name="TDispatcher">This is the Type of the Dispatcher interface to be added to the pipeline builder. This method expects a interface Type that will be implemented by Dispatcher.</typeparam>
+    /// <param name="options">DispatcherOptions instance defining the options for dispatcher.</param>
+    /// <returns>An IDispatcherBuilder instance that allows for further pipeline configuration.</returns>
+    /// <exception cref="Pipelines.Builder.Validators.Shared.InterfaceConstraint.Exceptions.ProvidedTypeIsNotInterfaceException"></exception>
+    /// <exception cref="Pipelines.Builder.Validators.Shared.OnlyOneHandleMethod.Exceptions.HandlerMethodNotFoundException"></exception>
+    /// <exception cref="Pipelines.Builder.Validators.Shared.OnlyOneHandleMethod.Exceptions.MultipleHandlerMethodsException"></exception>
+    /// <exception cref="Pipelines.Builder.Validators.Shared.MethodWithOneParameter.Exceptions.MethodShouldHaveAtLeastOneParameterException"></exception>
+    /// <exception cref="Pipelines.Builder.Validators.Dispatcher.InputType.Exceptions.DispatcherMethodInputTypeMismatchException"></exception>
+    /// <exception cref="Pipelines.Builder.Validators.Shared.MethodResultTypes.Exceptions.ExpectedMethodWithResultException"></exception>
+    /// <exception cref="Pipelines.Builder.Validators.Shared.MethodResultTypes.Exceptions.ExpectedVoidMethodException"></exception>
+    /// <exception cref="Pipelines.Builder.Validators.Shared.MethodResultTypes.Exceptions.ResultTypeCountMismatchException"></exception>
+    /// <exception cref="Pipelines.Builder.Validators.Shared.ShouldHaveClassConstraint.Exceptions.ReturnTypesShouldHaveClassConstraintException"></exception>
+    /// <exception cref="Pipelines.Builder.Validators.Shared.CompareTypes.Exceptions.GenericTypeCountMismatchException"></exception>
+    /// <exception cref="Pipelines.Builder.Validators.Shared.CompareTypes.Exceptions.GenericTypeMismatchException"></exception>
+    /// <exception cref="Pipelines.Builder.Validators.Shared.CompareTypes.Exceptions.IsGenericMismatchException"></exception>
+    /// <exception cref="Pipelines.Builder.Validators.Shared.CompareTypes.Exceptions.TypeMismatchException"></exception>
     public IPipelineDecoratorBuilder AddDispatcher<TDispatcher>(DispatcherOptions options) where TDispatcher : class;
 }
