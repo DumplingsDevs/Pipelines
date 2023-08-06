0b58dcf54ae45ad14989fe4f8651c3e1387720f3
KmLPro <kamil.bytner@gmail.com>
2023-08-06 22:51:26 +0200

    Add documentation for AddInput Method

--- src/Pipelines/Builder/Interfaces/IInputBuilder.cs
+++ src/Pipelines/Builder/Interfaces/IInputBuilder.cs
@@ -2,5 +2,13 @@ namespace Pipelines.Builder.Interfaces;
 
 public interface IInputBuilder
 {
+    /// <summary>
+    /// Adds a specific Input type to the pipeline builder. This interface will be used for Inputs in Handlers.
+    /// This is key for dynamically stitching different Input types, allowing system extendability and flexibility.
+    /// </summary>
+    /// <param name="type">This is the Type of the Input interface to be added to the pipeline builder. This method expects a interface Type that will be implemented by Inputs for Handlers</param>
+    /// <returns>Returns an instance of IHandlerBuilder, allowing for fluent chaining of configuration methods.</returns>
+    /// <exception cref="Pipelines.Builder.Validators.Shared.InterfaceConstraint.Exceptions.ProvidedTypeIsNotInterfaceException"></exception>
+    /// <exception cref="Pipelines.Builder.Validators.Shared.ShouldHaveClassConstraint.Exceptions.ReturnTypesShouldHaveClassConstraintException"></exception>
     public IHandlerBuilder AddInput(Type type);
 }
