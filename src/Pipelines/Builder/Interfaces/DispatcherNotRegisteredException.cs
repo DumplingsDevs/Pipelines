d489d083a94b2976b5e0d1d4a3cea008d714d1c8
Karasini <Karasini@users.noreply.github.com>
2023-08-06 15:48:31 +0200

    Move project into proper directories

--- src/Pipelines/Builder/Interfaces/DispatcherNotRegisteredException.cs
+++ src/Pipelines/Builder/Interfaces/DispatcherNotRegisteredException.cs
@@ -3,7 +3,7 @@ namespace Pipelines.Builder.Interfaces;
 public class DispatcherNotRegisteredException : Exception
 {
     private static readonly string ErrorMessage =
-        "There is not dispatcher registration in DI of {0} type. Probably something went wrong with source generator. You can make workaround and use Proxy dispatcher - set UseReflectionProxyImplementation to true when calling AddDispatcher.";
+        "There is not dispatcher registration in DI of {0} type. Make sure that Pipelines.Generator is added to your project. If you do not want to use Pipelines.Generators you can use Proxy dispatcher - set UseReflectionProxyImplementation to true when calling AddDispatcher.";
 
     public DispatcherNotRegisteredException(Type dispatcherType) : base(string.Format(ErrorMessage, dispatcherType.Name))
     {
