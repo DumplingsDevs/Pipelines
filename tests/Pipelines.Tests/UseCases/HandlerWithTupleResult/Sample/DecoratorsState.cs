524063d0139448b428e980aca0a33f8da70ffc69
KmLPro <kamil.bytner@gmail.com>
2023-08-06 21:56:13 +0200

    Added decorators for HandlerWithTupleResult

---
+++ tests/Pipelines.Tests/UseCases/HandlerWithTupleResult/Sample/DecoratorsState.cs
@@ -0,0 +1,6 @@
+namespace Pipelines.Tests.UseCases.HandlerWithTupleResult.Sample;
+
+public sealed class DecoratorsState
+{
+    public readonly List<string> Status = new();
+}
--- tests/Pipelines.Tests/UseCases/HandlerWithTupleResult/Tests.cs
+++ tests/Pipelines.Tests/UseCases/HandlerWithTupleResult/Tests.cs
@@ -7,16 +7,27 @@ public class Tests
 {
     private readonly ICommandDispatcher _commandDispatcher;
     private readonly DependencyContainer _dependencyContainer;
+    private readonly DecoratorsState _state;
+
     public Tests()
     {
         _dependencyContainer = new DependencyContainer();
         var assembly = typeof(DependencyContainer).Assembly;
-        
-        _dependencyContainer.RegisterPipeline<ICommandDispatcher>(assembly,typeof(ICommand<,>), typeof(ICommandHandler<,,>));
+
+        _dependencyContainer.RegisterPipeline<ICommandDispatcher>(assembly, typeof(ICommand<,>),
+            typeof(ICommandHandler<,,>), builder =>
+            {
+                builder
+                    .WithOpenTypeDecorator(typeof(LoggingDecorator<,,>));
+            });
+
+        _dependencyContainer.RegisterSingleton<DecoratorsState>();
+
         _dependencyContainer.BuildContainer();
         _commandDispatcher = _dependencyContainer.GetService<ICommandDispatcher>();
+        _state = _dependencyContainer.GetService<DecoratorsState>();
     }
-    
+
     [Test]
     public void HappyPath()
     {
@@ -25,9 +36,15 @@ public class Tests
 
         //Act
         var result = _commandDispatcher.SendAsync(request, new CancellationToken());
-            
+
         //Assert
         Assert.That(result.Item1.Value, Is.EqualTo("My test request"));
         Assert.That(result.Item2.Value, Is.EqualTo("Value"));
+        
+        CollectionAssert.AreEqual(new List<string>
+        {
+            typeof(LoggingDecorator<,,>).Name,
+            typeof(LoggingDecorator<,,>).Name,
+        }, _state.Status);
     }
 }
