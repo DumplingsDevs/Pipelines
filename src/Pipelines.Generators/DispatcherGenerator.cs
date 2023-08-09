38fa6a099ce50a0c5905d67ee9e1a7f1af17f0e0
Karasini <Karasini@users.noreply.github.com>
2023-08-09 23:29:08 +0200

    Move syntax receiver to new file

--- src/Pipelines.Generators/DispatcherGenerator.cs
+++ src/Pipelines.Generators/DispatcherGenerator.cs
@@ -8,6 +8,7 @@ using Microsoft.CodeAnalysis.Text;
 using Pipelines.Generators.Builders;
 using Pipelines.Generators.Models;
 using Pipelines.Generators.Extensions;
+using Pipelines.Generators.Syntax;
 
 #pragma warning disable RS1035 // Do not use banned APIs for analyzers
 
@@ -121,41 +122,5 @@ public class DispatcherGenerator : ISourceGenerator
     public void Initialize(GeneratorInitializationContext context)
     {
         // Tutaj można zarejestrować wszelkie niezbędne metadane
-    }
-
-    private class SyntaxReceiver : CSharpSyntaxWalker
-    {
-        private readonly Compilation _compilation;
-
-        public SyntaxReceiver(Compilation compilation, SyntaxWalkerDepth depth = SyntaxWalkerDepth.Node) : base(depth)
-        {
-            _compilation = compilation;
-        }
-
-        public InvocationExpressionSyntax AddInputInvocation { get; private set; }
-        public InvocationExpressionSyntax AddHandlerInvocation { get; private set; }
-        public InvocationExpressionSyntax AddDispatcherInvocation { get; private set; }
-
-        public override void VisitInvocationExpression(InvocationExpressionSyntax node)
-        {
-            // Visit each invocation expression and check if it matches our methods
-            var methodSymbol =
-                ModelExtensions.GetSymbolInfo(_compilation.GetSemanticModel(node.SyntaxTree), node).Symbol as
-                    IMethodSymbol;
-            if (methodSymbol?.Name == "AddInput")
-            {
-                AddInputInvocation = node;
-            }
-            else if (methodSymbol?.Name == "AddHandler")
-            {
-                AddHandlerInvocation = node;
-            }
-            else if (methodSymbol?.Name == "AddDispatcher" && methodSymbol.IsGenericMethod)
-            {
-                AddDispatcherInvocation = node;
-            }
-
-            base.VisitInvocationExpression(node);
-        }
     }
 }
