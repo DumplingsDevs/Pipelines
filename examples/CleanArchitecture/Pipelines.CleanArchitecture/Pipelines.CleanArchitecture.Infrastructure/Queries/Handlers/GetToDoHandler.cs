9de0a73e635ddb2fbd182465f37bec26867d3005
KmLPro <kamil.bytner@gmail.com>
2023-09-04 23:22:14 +0200

    [Example] Added Query Handler for Get To Do

---
+++ examples/CleanArchitecture/Pipelines.CleanArchitecture/Pipelines.CleanArchitecture.Infrastructure/Queries/Handlers/GetToDoHandler.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Pipelines.CleanArchitecture.Abstractions.Queries;
+using Pipelines.CleanArchitecture.Application.Queries;
+using Pipelines.CleanArchitecture.Infrastructure.Persistance;
+
+namespace Pipelines.CleanArchitecture.Infrastructure.Queries.Handlers;
+
+public class GetToDoHandler : IQueryHandler<GetToDoQuery, GetToDoResult>
+{
+    private readonly ToDoDbContext _dbContext;
+
+    public GetToDoHandler(ToDoDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<GetToDoResult> HandleAsync(GetToDoQuery query, CancellationToken token)
+    {
+        var toDo = await _dbContext.ToDos.FirstAsync(cancellationToken: token);
+        return new GetToDoResult(toDo.Id, toDo.Title);
+    }
+}
