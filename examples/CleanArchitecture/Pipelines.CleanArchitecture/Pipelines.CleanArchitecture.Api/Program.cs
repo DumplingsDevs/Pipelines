08e9e84d95db93941d3c93d0f41aaf0e67cbd532
KmLPro <kamil.bytner@gmail.com>
2023-09-04 23:13:42 +0200

    [Example] Added swagger

--- examples/CleanArchitecture/Pipelines.CleanArchitecture/Pipelines.CleanArchitecture.Api/Program.cs
+++ examples/CleanArchitecture/Pipelines.CleanArchitecture/Pipelines.CleanArchitecture.Api/Program.cs
@@ -3,6 +3,12 @@ using Pipelines.CleanArchitecture.Api;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("./swagger/v1/swagger.json", "Pipelines Example");
+    c.RoutePrefix = string.Empty;
+});
 
 app.RegisterEndpoints();
 app.Run();
